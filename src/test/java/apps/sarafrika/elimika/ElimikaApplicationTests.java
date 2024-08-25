19f6e1f255f77e29b6d3ff5d3d5f4c64d77e5298
Willis <mwangiwilly395@gmail.com>
2024-08-25 11:27:40 +0300

    Initial Commit

---
+++ src/test/java/apps/sarafrika/elimika/ElimikaApplicationTests.java
@@ -0,0 +1,13 @@
+package apps.sarafrika.elimika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElimikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
