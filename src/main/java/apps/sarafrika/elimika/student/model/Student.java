fc01ba78310684aa539cc7c30c582a09f7f4fc12
Wilfred Mwangi <wilfred.mwangi@kyosk.app>
2025-04-16 16:26:10 +0300

    fix: mapping instructor fields at the entity class level

--- src/main/java/apps/sarafrika/elimika/student/model/Student.java
+++ src/main/java/apps/sarafrika/elimika/student/model/Student.java
@@ -17,6 +17,8 @@ import java.util.UUID;
 public class Student extends BaseEntity {
     @Column(name = "user_uuid")
     private UUID userUuid;
+    @Column(name = "full_name")
+    private String fullName;
     @Column(name = "guardian_1_name")
     private String firstGuardianName;
     @Column(name = "guardian_1_mobile")
