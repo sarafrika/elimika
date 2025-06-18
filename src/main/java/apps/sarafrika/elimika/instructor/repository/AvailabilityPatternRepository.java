8d4a0d67a1aeb53b819ee7f8664338f0b93b7566
Wilfred Njuguna <mwangiwilly395@gmail.com>
2025-06-19 02:47:05 +0300

    chore!: removal of availability pattern

--- src/main/java/apps/sarafrika/elimika/instructor/repository/AvailabilityPatternRepository.java
+++
@@ -1,7 +0,0 @@
-package apps.sarafrika.elimika.instructor.repository;
-
-import apps.sarafrika.elimika.instructor.model.AvailabilityPattern;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AvailabilityPatternRepository extends JpaRepository<AvailabilityPattern, Long> {
-}
